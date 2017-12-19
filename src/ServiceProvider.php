0a1806f4a45b0cbb10a7ba56f27c2af4d520c896
Evgeniy Marthenov <evgeniy.marthenov@gmail.com>
2017-12-19 14:07:58 +0300

    Update ServiceProvider.php
    Fix for lumen

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,14 +3,26 @@
 namespace Friday14\Beanstalk;
 
 use Friday14\Beanstalk\Console\BeanstalkCommand;
+use Friday14\Beanstalk\Queue\BeanstalkWorker;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\ServiceProvider as SProvider;
 
 class ServiceProvider extends SProvider
 {
-    public function register()
+    public function boot()
     {
+        $this->registerWorker();
         $this->commands([
             BeanstalkCommand::class
         ]);
+    }
+
+    public function registerWorker()
+    {
+        $this->app->singleton('queue.beanstalk.worker', function () {
+            return new BeanstalkWorker(
+                $this->app['queue'], $this->app['events'], $this->app[ExceptionHandler::class]
+            );
+        });
     }
 }
